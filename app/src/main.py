0d54e7c61601bc731eb14bd69fea4032e2017d8e
MarinhoCM <marinhoc384@gmail.com>
2024-04-10 20:55:46 -0300

    :rocket: deploy: final commit

--- app/src/main.py
+++ app/src/main.py
@@ -1,11 +1,21 @@
 import os, sys
 from unidecode import unidecode
 
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtGui import QFont
+
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(SCRIPT_DIR))
 
-# from helpers.LogConfig import LogConfig
-# from helpers.Words import Words
+from resource.templates.MainWindow import Ui_CommentsReviewer
+
+from helpers.LogConfig import LogConfig
 
 if __name__ == "__main__":
-    pass
+    LogConfig()
+    app = QtWidgets.QApplication(sys.argv)
+    main_wnd = QtWidgets.QMainWindow()
+    ui = Ui_CommentsReviewer()
+    ui.setupUi(main_wnd)
+    main_wnd.show()
+    sys.exit(app.exec_())
