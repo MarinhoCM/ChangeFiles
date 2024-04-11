b1b35747d205dbb93f556a902f2942f810159bd7
MarinhoCM <marinhoc384@gmail.com>
2024-04-10 21:05:50 -0300

    :rocket: deploy: final commit

--- app/src/main.py
+++ app/src/main.py
@@ -1,21 +1,27 @@
 import os, sys
-from unidecode import unidecode
-
-from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtGui import QFont
+from PyQt5 import QtWidgets
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(SCRIPT_DIR))
 
+from helpers.LogConfig import LogConfig
 from resource.templates.MainWindow import Ui_CommentsReviewer
 
-from helpers.LogConfig import LogConfig
+
+def start() -> None:
+    try:
+        app = QtWidgets.QApplication(sys.argv)
+        main_wnd = QtWidgets.QMainWindow()
+        ui = Ui_CommentsReviewer()
+        ui.setupUi(main_wnd)
+        main_wnd.show()
+        sys.exit(app.exec_())
+    except Exception as e:
+        raise Exception(
+            f"Ocorreu um erro ao tentar inicializar interface. \nError: {e}"
+        )
+
 
 if __name__ == "__main__":
     LogConfig()
-    app = QtWidgets.QApplication(sys.argv)
-    main_wnd = QtWidgets.QMainWindow()
-    ui = Ui_CommentsReviewer()
-    ui.setupUi(main_wnd)
-    main_wnd.show()
-    sys.exit(app.exec_())
+    start()
